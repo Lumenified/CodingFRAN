0acb8978f6a8b4e988894cc0f326faa547e2f751
Lumenified <tahsinsaid.turan@gazi.edu.tr>
2019-07-24 22:40:55 +0300

    Breadth-First Search

--- Breadh-First/sketch.js
+++ Breadh-First/sketch.js
@@ -1,6 +1,36 @@
-function setup() {
-	createCanvas(windowWidth, windowHeight);
+var data;
+var graph;
+function preload() {
+  data = loadJSON('kevinbacon.json');
 }
+
+function setup() {
+  graph = new Graph();
+  noCanvas();
+  //console.log(data);
+
+  var movies = data.movies;
+
+  for (var i = 0; i < movies.length; i++){
+    var movie = movies[i].title;
+    var cast = movies[i].cast;
+    var movieNode = new Node(movie);
+    graph.addNode(movieNode);
+
+    for (var j = 0;j < cast.length; j++){
+      var actor = cast[j];
+      var actorNode = graph.getNode(actor);
+      if (actorNode == undefined){
+        actorNode = new Node(actor);
+      }
+      graph.addNode(actorNode);
+      movieNode.addEdge(actorNode);
+      //console.log(actor);
+    }
+  }
+  console.log(graph);
+}
+
 
 function draw() {
 
