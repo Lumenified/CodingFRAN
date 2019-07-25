36c27b73a39fe0baf0a2dce2c94cbe3af9d71407
Lumenified <tahsinsaid.turan@gazi.edu.tr>
2019-07-25 22:33:13 +0300

    neighbours added

--- Maze-Generator/sketch.js
+++ Maze-Generator/sketch.js
@@ -1,7 +1,7 @@
 var cols, rows;
 var w = 40;
 var grid = [];
-
+var current;
 function setup() {
   createCanvas(400, 400);
   cols = floor(width / w);
@@ -12,18 +12,58 @@ function setup() {
       grid.push(cell);
     }
   }
+  current = grid[0];
 }
 
 function draw() {
   background(51);
+  frameRate(5);
   for (var i = 0; i < grid.length; i++) {
     grid[i].show()
   }
+  current.visited = true;
+  var next = current.checkNeighbors();
+  if (next){
+    next.visited = true;
+    current = next;
+  }
 }
 
+function index(i,j){
+  if ( i < 0 || j < 0 || i > cols-1 || j > rows -1){
+    return -1;
+  }
+  return i + j * cols;
+}
 function Cell(i, j) {
   this.i = i;
   this.j = j;
+  this.visited = false;
+  this.checkNeighbors = function(){
+    var neighbours = [];
+    var top     = grid[index(i,    j-1)];
+    var right   = grid[index(i+1,  j)];
+    var left    = grid[index(i-1,  j)];
+    var bottom  = grid[index(i,    j+1)];
+    if (top && !top.visited){
+      neighbours.push(top);
+    }
+    if (right && !right.visited){
+      neighbours.push(right);
+    }
+    if (left && !left.visited){
+      neighbours.push(left);
+    }
+    if (bottom && !bottom.visited){
+      neighbours.push(bottom);
+    }
+    if (neighbours.length > 0){
+      var r = floor(random(0, neighbours.length));
+      return neighbours[r];
+    } else {
+      return undefined;
+    }
+  }
   this.show = function() {
     var x = this.i * w;
     var y = this.j * w;
@@ -41,8 +81,10 @@ function Cell(i, j) {
     if (this.walls[3]) {
       line(x, y + w, x, y);
     }
+    if (this.visited){
 
-    // noFill();
-    // rect(x, y, w, w);
+      fill(255,0,255,100);
+      rect(x, y, w, w);
+    }
   }
 }
