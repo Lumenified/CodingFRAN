ee435eb445788ea0e930b3f20033654e58014a12
Lumenified <tahsinsaid.turan@gazi.edu.tr>
2019-07-27 23:45:40 +0300

    pathfinding

--- AStar-Pathfinding/sketch.js
+++ AStar-Pathfinding/sketch.js
@@ -22,39 +22,6 @@ function heuristic(a, b){
 	return d;
 }
 
-function Spot(i, j) {
-  this.i = i;
-  this.j = j;
-  this.f = 0;
-  this.g = 0;
-  this.h = 0;
-	this.previous = undefined;
-  this.neighbours = [];
-  this.show = function(col) {
-    fill(col);
-    noStroke();
-    rect(this.i * w, this.j * h, w - 1, h - 1);
-  }
-  this.addNeighbours = function(grid) {
-    var i = this.i;
-    var j = this.j;
-    if (i < cols - 1) {
-
-      this.neighbours.push(grid[i + 1][j]);
-    }
-    if (i > 0) {
-
-      this.neighbours.push(grid[i - 1][j]);
-    }
-    if (j < rows - 1) {
-
-      this.neighbours.push(grid[i][j + 1]);
-    }
-    if (j > 0) {
-      this.neighbours.push(grid[i][j - 1]);
-    }
-  }
-}
 
 function setup() {
   createCanvas(400, 400);
